57849c88fc0e86869032078a5dec14eb32d22963
raluber <rluque.ext@catec.aero>
2020-03-10 11:06:47 +0100

    change proyector by projector and additional functions sorting as myself logic

--- src/zlaser_sdk_ros/projection_node.py
+++ src/zlaser_sdk_ros/projection_node.py
@@ -21,12 +21,13 @@ class ProjectionNode:
         self.lic_path = self.pkg_path + "/lic/1900027652.lic"
 
         # Create projector object
-        self.proyector = ProjectorManager()
+        self.projector = ProjectorManager()
 
         # Open services
         self.cnt_srv     = rospy.Service('/projector_srv/connect', Trigger, self.connectionCb)
-        self.discnt_srv  = rospy.Service('/projector_srv/disconnect', Trigger, self.disconnectionCb)
         self.lic_srv     = rospy.Service('/projector_srv/load_license', Trigger, self.transferLicenseCb)
+        self.discnt_srv  = rospy.Service('/projector_srv/disconnect', Trigger, self.disconnectionCb)
+        
         self.setup_srv   = rospy.Service('/projector_srv/setup', Trigger, self.setupCb)
         self.cs_srv      = rospy.Service('/projector_srv/cs', Trigger, self.defineCoordSysCb)
         self.project_srv = rospy.Service('/projector_srv/project', ProjectionShape, self.projectionCb)
@@ -37,15 +38,21 @@ class ProjectionNode:
 
     def connectionCb(self,req):
         rospy.loginfo("Received request to start projector")
-        e = self.proyector.clientServerConnect()
+        e = self.projector.clientServerConnect()
         rospy.loginfo(e)
-        e = self.proyector.activate()
+        e = self.projector.activate()
         rospy.loginfo(e)
         return TriggerResponse(True,str(e))
     
+    def transferLicenseCb(self,req):
+        self.projector.license_path = self.lic_path
+        e = self.projector.transferLicense()
+        rospy.loginfo(e)
+        return TriggerResponse(True,"license loaded")
+
     def disconnectionCb(self,req):
         rospy.loginfo("Received request to stop projector")
-        e = self.proyector.deactivate()
+        e = self.projector.deactivate()
         rospy.loginfo(e)
         return TriggerResponse(True,str(e))
 
@@ -55,46 +62,40 @@ class ProjectionNode:
     def setNewCoordSystem(self,cs):
         if len(cs)>1:
             rospy.loginfo("Setting {} as default coordinate system".format(cs[-1]))
-            self.proyector.setCoordinateSystem(cs[-1])
+            self.projector.setCoordinateSystem(cs[-1])
 
     def setupCb(self,req):
         rospy.loginfo("Received request to setup projector")
         # connect to service
-        e = self.proyector.clientServerConnect()
+        e = self.projector.clientServerConnect()
         rospy.loginfo(e)
         # activate projector
-        e = self.proyector.activate()
+        e = self.projector.activate()
         rospy.loginfo(e)
         # check license
-        if not self.proyector.checkLicense():
+        if not self.projector.checkLicense():
             rospy.logwarn("License is not valid. Load a new one: \n\n rosservice call /projector_srv/load_license")      
             return TriggerResponse(False,"end setup")    
         else:
             rospy.loginfo("License is valid...")
             # check coordinate system
-            cs = self.proyector.getCoordinateSystems()
+            cs = self.projector.getCoordinateSystems()
             rospy.loginfo("Available coordinate systems: {}".format(cs))
             # save coordinate system as default
             self.setNewCoordSystem(cs)
         return TriggerResponse(True,"end setup")
 
-    def transferLicenseCb(self,req):
-        self.proyector.license_path = self.lic_path
-        e = self.proyector.transferLicense()
-        rospy.loginfo(e)
-        return TriggerResponse(True,"license loaded")
-
     def defineCoordSysCb(self,req):
         rospy.loginfo("Received request to create new coordinate system. Please wait for the system to indicate the end")
         # define and register coordinate system
-        self.proyector.do_register_coordinate_system = True
-        e = self.proyector.defineCoordinateSystem("small_pieza")
+        self.projector.do_register_coordinate_system = True
+        e = self.projector.defineCoordinateSystem("small_pieza")
         rospy.loginfo(e)
         # show created coordinate system
-        cs = self.proyector.getCoordinateSystems()
+        cs = self.projector.getCoordinateSystems()
         rospy.loginfo("Available coordinate systems: {}".format(cs))
         rospy.loginfo("Projecting {} coordinate system".format(cs[-1]))
-        self.proyector.showCoordinateSystem(5)
+        self.projector.showCoordinateSystem(5)
         # save new coordinate system as default
         self.setNewCoordSystem(cs)
         return TriggerResponse(True,"Created coordinate system")            
@@ -109,26 +110,26 @@ class ProjectionNode:
         shape = req.shape.data 
         if shape == "circle":
             rospy.loginfo("Creating circle shape")
-            e = self.proyector.createCircle(x,y,r,id)
+            e = self.projector.createCircle(x,y,r,id)
             rospy.loginfo(e)
             # start projection until stop service is called
             rospy.loginfo(" - Projecting - ")
-            self.proyector.startProjection()
+            self.projector.startProjection()
 
         elif shape == "cross":
             rospy.loginfo("Creating cross shape")
-            e = self.proyector.createCross(x,y,r,id)
+            e = self.projector.createCross(x,y,r,id)
             rospy.loginfo(e)
             # start projection until stop service is called
             rospy.loginfo(" - Projecting - ")
-            self.proyector.startProjection()  
+            self.projector.startProjection()  
         else: 
             return ProjectionShapeResponse(Bool(False))
         return ProjectionShapeResponse(Bool(True))
 
     def projectionStopCb(self,req):
         # stop projection when service is called 
-        self.proyector.stopProjection()
+        self.projector.stopProjection()
         return TriggerResponse(True,"Stopped")
 
 def main():
