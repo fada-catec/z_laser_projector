191be3dbf77a0814586888e62a5a41ca23354522
rafluqber <rluque@catec.aero>
2020-06-24 16:36:49 +0200

    LICENSE Apache 2.0 added

--- src/zlaser_sdk_ros/projector_manager.py
+++ src/zlaser_sdk_ros/projector_manager.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright [yyyy] [name of copyright owner]
+# Copyright (c) 2019, FADA-CATEC
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- src/zlaser_sdk_ros/zlp.py
+++ src/zlaser_sdk_ros/zlp.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#!/usr/bin/env python3
+
+# Copyright (c) 2019, FADA-CATEC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Helper module for python thrift interface to ZLP Service. 
 This module contains utility classes and methods which ease the usage of the thrift interface to ZLP Service."""
 
